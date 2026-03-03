ba167630d01c35e232b6d689f0831d763cff4a23
vandoops <100371881+vandoops@users.noreply.github.com>
2026-03-03 19:03:19 -0300

    melhorias

--- sw.js
+++ sw.js
@@ -33,13 +33,4 @@ self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(resp => resp || fetch(event.request))
   );
-
-  self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(resp => {
-      // Se não achar no cache, tenta buscar
-      return resp || fetch(event.request).catch(() => caches.match("/ranking.html"));
-    })
-  );
-});
 });
