14a6eca63e644c5180c344e1539abaeb196c57de
vandoops <100371881+vandoops@users.noreply.github.com>
2026-03-03 18:50:23 -0300

    melhorias

--- sw.js
+++ sw.js
@@ -1,5 +1,36 @@
+const CACHE_NAME = "bruxaria-cache-v1";
+const urlsToCache = [
+  "/", 
+  "/ranking.html", 
+  "/galeria.html", 
+  "/style-ranking.css", 
+  "/style.css", 
+  "/logopng.png", 
+  "/galeriafundo.jpeg"
+];
+
 self.addEventListener("install", event => {
   console.log("Service Worker instalado");
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener("fetch", event => {});
+self.addEventListener("activate", event => {
+  console.log("Service Worker ativado");
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
